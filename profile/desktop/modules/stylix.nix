9fa4852eabc352e97695633476d1fe5a767b2256
meowfeet <279364940+meowfeet@users.noreply.github.com>
2026-05-04 22:40:09 +0200

    phase 19

--- profile/desktop/modules/stylix.nix
+++ profile/desktop/modules/stylix.nix
@@ -8,6 +8,12 @@
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
+
     fonts.sizes.popups = 14;
 
     opacity = {
