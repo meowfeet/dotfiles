900e7c2961bd82f6e1a3ea8f6b7972f05d979c88
meowfeet <279364940+meowfeet@users.noreply.github.com>
2026-05-06 09:03:29 +0200

    phase 11 cosmic

--- profile/desktop/modules/hide.nix
+++ profile/desktop/modules/hide.nix
@@ -1,32 +1,25 @@
-{ lib, pkgs, ... }:
+{ ... }:
 
-let
-  entriesFrom =
-    pkg:
-    map (lib.removeSuffix ".desktop")
-        (builtins.attrNames (builtins.readDir "${pkg}/share/applications"));
-
-  hide =
-    [ ]
-    ++ entriesFrom pkgs.cosmic-applets
-    ++ entriesFrom pkgs.cosmic-applibrary
-    ++ entriesFrom pkgs.cosmic-bg
-    ++ entriesFrom pkgs.cosmic-launcher
-    ++ entriesFrom pkgs.cosmic-notifications
-    ++ entriesFrom pkgs.cosmic-settings
-    ++ entriesFrom pkgs.cosmic-workspaces-epoch;
-    # ++ [
-    #   "geoclue-where-am-i"
-    #   "nm-applet"
-    #   "nm-connection-editor"
-    #   "nvidia-settings"
-    #   "org.freedesktop.Xwayland"
-    #   "xdg-desktop-portal-gtk"
-    # ];
-in
 {
-  hm.xdg.desktopEntries = lib.genAttrs hide (name: {
-    inherit name;
-    noDisplay = true;
-  });
+  nixpkgs.overlays = [
+    (_: prev: {
+      pop-launcher = prev.pop-launcher.overrideAttrs (old: {
+        postInstall = (old.postInstall or "") + ''
+          rm -rf $out/share/pop-launcher/plugins/{pulse,scripts}
+        '';
+      });
+    })
+  ];
+
+  environment.extraSetup = ''
+    shopt -s nullglob
+    for link in $out/share/applications/*.desktop; do
+      [ -L "$link" ] || continue
+      target=$(readlink -f "$link")
+      rm "$link"
+
+      sed '/^OnlyShowIn=/d;/^NoDisplay=/d' "$target" > "$link"
+      echo 'NoDisplay=true' >> "$link"
+    done
+  '';
 }
